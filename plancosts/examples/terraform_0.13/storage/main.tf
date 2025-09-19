9e4bfe158b6ca85fb2923d8c938ca9d70c1a2d51
Nikhil Amala Jerrin J <jerrin.james@tu-dortmund.de>
2025-09-19 19:28:40 +0200

    CI/CD test

--- plancosts/examples/terraform_0.13/storage/main.tf
+++ plancosts/examples/terraform_0.13/storage/main.tf
@@ -4,7 +4,7 @@ variable "availability_zone" {
 
 resource "aws_ebs_volume" "standard" {
   availability_zone = var.availability_zone
-  size              = 300
+  size              = 40
   type              = "standard"
 }
 
